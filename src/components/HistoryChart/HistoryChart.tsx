f2f2393af37991e65402c2cf4ea269c124b5b1b0
MKJ1337 <mik.jed2003@gmail.com>
2023-04-03 20:01:51 +0200

    update

--- src/components/HistoryChart/HistoryChart.tsx
+++ src/components/HistoryChart/HistoryChart.tsx
@@ -22,55 +22,94 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Filler,
-  Legend
+  Filler
 );
 
-export const HistoryChart = () => {
+interface HistoryChartProps {
+  value: string;
+}
+
+export const HistoryChart = ({ value }: HistoryChartProps) => {
   const { coin } = useParams();
   const [coinData, setCoinData] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchHistoryData = async () => {
+      setIsLoading(true);
       try {
         const { data } = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=14&interval=daily`
+          `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=${value}&interval=daily`
         );
         setCoinData(data);
+        setIsLoading(false);
       } catch (err) {
+        setIsLoading(false);
         console.log(err);
       }
     };
     fetchHistoryData();
-  }, []);
+  }, [value]);
 
   const coinChartData = coinData.prices?.map((value: any) => ({
     x: value[0],
     y: value[1].toFixed(2),
   }));
 
-  const options = {
-    responsive: true,
-  };
-
   const data = {
     labels: coinChartData?.map((value: any) =>
-      moment(value.x).format('MMM DD')
+      moment(value.x).format('MMM DD HH')
     ),
     datasets: [
       {
-        fill: true,
         label: coin,
         data: coinChartData?.map((val: any) => val.y),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: 'rgba(218,0,255,1)',
+        backgroundColor: 'transparent',
       },
     ],
   };
 
+  const options: any = {
+    plugins: {
+      legeng: false,
+    },
+    scales: {
+      y: {
+        ticks: {
+          font: {
+            size: '0px',
+          },
+        },
+      },
+      x: {
+        grid: {
+          borderDash: [6],
+          border: false,
+        },
+        ticks: {
+          font: {
+            size: '1px',
+          },
+        },
+      },
+    },
+  };
+
+  console.log(coinData);
+
+  if (isLoading) return <span>Loading...</span>;
+
   return (
-    <div>
-      <Line options={options} data={data} />
+    <div
+      style={{
+        width: '650px',
+        minHeight: '300px',
+        marginLeft: '50px',
+        paddingBottom: '50px',
+      }}
+    >
+      <Line data={data} options={options} />
     </div>
   );
 };
--- src/pages/SingleCoin/SingleCoin.tsx
+++ src/pages/SingleCoin/SingleCoin.tsx
@@ -1,16 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+
 
 // styles
 import './SingleCoin.scss';
 import { HistoryChart } from '../../components/HistoryChart/HistoryChart';
 
 export const SingleCoin = () => {
-
+  const [value, setValue] = useState<string>('1');
   return (
     <div className="single__coin">
-      <HistoryChart />
+      <HistoryChart value={value} />
+      <select onChange={(e) => setValue(e.target.value)}>
+        <option value={1}>day</option>
+        <option value={7}>week</option>
+        <option value={31}>month</option>
+        <option value={365}>year</option>
+      </select>
     </div>
   );
 };
