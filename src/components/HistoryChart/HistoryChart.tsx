715574ba58213f5cdc1cc4c0318bae927526ac46
MKJ1337 <mik.jed2003@gmail.com>
2023-04-03 12:43:40 +0200

    charts

---
+++ src/components/HistoryChart/HistoryChart.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const HistoryChart = () => {
+  const { coin } = useParams();
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const fetchHistoryData = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=7`
+        );
+        setCoinData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchHistoryData();
+  }, []);
+
+  const coinChartData = coinData.prices?.map((value) => ({
+    x: value[0],
+    y: value[1].toFixed(2),
+  }));
+
+  const options = {
+    responsive: true,
+  };
+
+  const data = {
+    labels: coinChartData?.map(value => moment(value.x).format('MMM DD')),
+    datasets: [
+      {
+        fill: true,
+        label: coin,
+        data: coinChartData?.map(val => val.y),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      }
+    ]
+  }
+
+  return <div>
+     <Line options={options} data={data} />
+  </div>;
+};
--- src/pages/SingleCoin/SingleCoin.tsx
+++ src/pages/SingleCoin/SingleCoin.tsx
@@ -4,28 +4,13 @@ import axios from 'axios';
 
 // styles
 import './SingleCoin.scss';
+import { HistoryChart } from '../../components/HistoryChart/HistoryChart';
 
 export const SingleCoin = () => {
-  const [coin, setCoin] = useState({});
-  const coinId = useLocation().pathname.split('/')[2];
 
-  console.log(location);
-
-  useEffect(() => {
-    const fetchSingleCoin = async () => {
-      try {
-        const { data } = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${coinId}`
-        );
-        setCoin(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchSingleCoin();
-  }, []);
-
-  console.log(coin);
-
-  return <div>SingleCoin</div>;
+  return (
+    <div className="single__coin">
+      <HistoryChart />
+    </div>
+  );
 };
